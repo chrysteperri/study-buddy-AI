f8f47fed90f5a168df5504e6cecd10ca95ee1552
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-02-05 22:11:30 +0300

    ci: configure prettier, add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,29 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 
-import { cn } from '@/lib/utils';
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
 
-import '@/styles/globals.css';
+import "@/styles/globals.css";
 
 const fontRegularSans = localFont({
-  src: '../assets/fonts/Inter-Regular.ttf',
-  variable: '--font-sans',
+  src: "../assets/fonts/Inter-Regular.ttf",
+  variable: "--font-sans",
 });
 
 const fontBoldSans = localFont({
-  src: '../assets/fonts/Inter-Regular.ttf',
-  variable: '--font-BoldSans',
+  src: "../assets/fonts/Inter-Regular.ttf",
+  variable: "--font-BoldSans",
 });
 
 const fontHeading = localFont({
-  src: '../assets/fonts/CalSans-SemiBold.woff2',
-  variable: '--font-heading',
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
 });
 
 export const metadata: Metadata = {
-  title: 'Study Buddy AI',
-  description: 'Your AI study partner',
+  title: "Study Buddy AI",
+  description: "Your AI study partner",
 };
 
 interface RootLayoutProps {
@@ -35,15 +36,15 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body
         className={cn(
-          'min-h-screen font-sans_regular antialiased',
+          "min-h-screen font-sans_regular antialiased",
           fontRegularSans.variable,
           fontBoldSans.variable,
           fontHeading.variable
         )}
       >
+        <Toaster />
         {children}
       </body>
-      <footer></footer>
     </html>
   );
 }
