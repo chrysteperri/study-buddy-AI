619e9eee0f5dc53cc34f6f4bdbb22825fde1be15
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2024-03-17 21:40:06 +0300

    style: change theme-color

--- components/chat/chat-wrapper.tsx
+++ components/chat/chat-wrapper.tsx
@@ -33,7 +33,7 @@ const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
       <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
         <div className="mb-28 flex flex-1 flex-col items-center justify-center">
           <div className="flex flex-col items-center gap-2">
-            <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
+            <Loader2 className="h-8 w-8 animate-spin text-primary/50" />
             <h3 className="text-xl font-semibold">Loading...</h3>
             <p className="text-sm text-zinc-500">
               We&apos;re preparing your PDF.
@@ -50,7 +50,7 @@ const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
       <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
         <div className="mb-28 flex flex-1 flex-col items-center justify-center">
           <div className="flex flex-col items-center gap-2">
-            <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
+            <Loader2 className="h-8 w-8 animate-spin text-primary/50" />
             <h3 className="text-xl font-semibold">Processing PDF...</h3>
             <p className="text-sm text-zinc-500">This won&apos;t take long.</p>
           </div>
